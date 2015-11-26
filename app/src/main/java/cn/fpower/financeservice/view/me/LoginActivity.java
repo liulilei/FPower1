e6a8dcd446b978a870d99c3447efffca2ef372ae
542910866@qq.com <lll2650761>
2015-11-27 00:20:16 +0800

    shide

---
+++ app/src/main/java/cn/fpower/financeservice/view/me/LoginActivity.java
@@ -0,0 +1,15 @@
+package cn.fpower.financeservice.view.me;
+
+import android.support.v4.app.FragmentActivity;
+
+import cn.fpower.financeservice.view.BaseActivity;
+
+/**
+ *  登陆
+ */
+public class LoginActivity extends BaseActivity {
+    @Override
+    protected int initLayout() {
+        return 0;
+    }
+}
