c8469c2e02340fb1defd13e07685ad8dfb47e1fd
542910866@qqcom <lll2650761>
2015-11-30 23:54:17 +0800

    成功案例界面，主界面

--- app/src/main/java/cn/fpower/financeservice/fragment/HomeFragment.java
+++ app/src/main/java/cn/fpower/financeservice/fragment/HomeFragment.java
@@ -1,5 +1,6 @@
 package cn.fpower.financeservice.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,11 +16,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 import cn.fpower.financeservice.R;
+import cn.fpower.financeservice.adapter.SuccessExampleAdapter;
+import cn.fpower.financeservice.constants.Constants;
 import cn.fpower.financeservice.manager.viewmanager.HomeMenuFragmentGroup;
 import cn.fpower.financeservice.mode.BannerInfo;
 import cn.fpower.financeservice.mode.BannerInfo.BannerList;
+import cn.fpower.financeservice.mode.SuccessExampleInfo;
+import cn.fpower.financeservice.mode.SuccessExampleInfo.SuccessExample;
+import cn.fpower.financeservice.view.WebViewActivity;
 import cn.fpower.financeservice.view.home.HomeActivity;
+import cn.fpower.financeservice.view.home.SuccessExampleActivity;
 import cn.fpower.financeservice.view.widget.HorizontalViewPager;
+import cn.fpower.financeservice.view.widget.OnClickInnerListView;
 
 /**
  * Created by ll on 2015/11/26.
@@ -50,11 +58,19 @@ public class HomeFragment extends BaseFragment implements View.OnClickListener {
     @ViewInject(R.id.fragment_home_add_entering_tv)
     private TextView enteringTv;
 
+    @ViewInject(R.id.fragment_home_success_lv)
+    private OnClickInnerListView successLv;
+
+    @ViewInject(R.id.fragment_home_success_more)
+    private TextView successMoreTv;
+
     private List<BannerList> bannerList;
 
     private String no_banner_url = "drawable://" + R.mipmap.ad1;
 
     private HomeMenuFragmentGroup homeMenuFragmentGroup;
+
+    private Intent intent;
 
     @Override
     protected ViewGroup onCreateView(LayoutInflater inflater, Bundle savedInstanceState) {
@@ -70,6 +86,7 @@ public class HomeFragment extends BaseFragment implements View.OnClickListener {
         loanTv.setOnClickListener(this);
         limitTv.setOnClickListener(this);
         enteringTv.setOnClickListener(this);
+        successMoreTv.setOnClickListener(this);
     }
 
     @Override
@@ -81,19 +98,38 @@ public class HomeFragment extends BaseFragment implements View.OnClickListener {
         bannerList.add(banner);
         homeMenuFragmentGroup = new HomeMenuFragmentGroup(getActivity());
         homeMenuFragmentGroup.initAd(bannerList, homeVp, pointLl);
+
+        List<SuccessExample> data = new ArrayList<>();
+        for (int i = 0; i < 6; i++) {
+            data.add(new SuccessExampleInfo().new SuccessExample());
+        }
+        successLv.setAdapter(new SuccessExampleAdapter(getActivity(), data));
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.fragment_home_interest_tv:
+                intent = new Intent(getActivity(), WebViewActivity.class);
+                intent.putExtra(Constants.ADDRESS, "http://www.baidu.com");
+                startActivity(intent);
                 break;
             case R.id.fragment_home_loan_tv:
+                intent = new Intent(getActivity(), WebViewActivity.class);
+                intent.putExtra(Constants.ADDRESS, "http://www.baidu.com");
+                startActivity(intent);
                 break;
             case R.id.fragment_home_limit_tv:
+                intent = new Intent(getActivity(), WebViewActivity.class);
+                intent.putExtra(Constants.ADDRESS, "http://www.baidu.com");
+                startActivity(intent);
                 break;
             case R.id.fragment_home_add_entering_tv:
                 ((HomeActivity) getActivity()).selectEnteringFragment();
+                break;
+            case R.id.fragment_home_success_more:
+                intent = new Intent(getActivity(), SuccessExampleActivity.class);
+                startActivity(intent);
                 break;
         }
     }
